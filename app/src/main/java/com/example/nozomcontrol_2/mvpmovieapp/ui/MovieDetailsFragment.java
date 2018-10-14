191812f3a4bde40c417cc550df08dd9456e42c4d
TareqFahim <tareq.mfahim95@gmail.com>
2018-10-14 19:56:32 +0200

    Movie Details view almost done

--- app/src/main/java/com/example/nozomcontrol_2/mvpmovieapp/ui/MovieDetailsFragment.java
+++ app/src/main/java/com/example/nozomcontrol_2/mvpmovieapp/ui/MovieDetailsFragment.java
@@ -18,6 +18,8 @@ import com.example.nozomcontrol_2.mvpmovieapp.data.MovieInfo;
 import com.example.nozomcontrol_2.mvpmovieapp.ui.presenters.MovieDetailsPresenter;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -28,12 +30,20 @@ import butterknife.ButterKnife;
 public class MovieDetailsFragment extends Fragment implements MovieDetailsPresenter.ViewUpdateCallback {
     @BindView(R.id.movie_details_fragment_collapsing_toolbar_imageview)
     ImageView collapsingToolbarImageView;
-    @BindView(R.id.movie_details_fragment_toolbar)
-    Toolbar movieDetailsFragmentToolbar;
     @BindView(R.id.movie_details_description_textView)
     TextView movieDetailsFragmentDescriptionTextView;
     @BindView(R.id.movie_details_fragment_collapsing_toolbar)
     CollapsingToolbarLayout movieDetailsCollapsingToolbar;
+    @BindView(R.id.movie_details_fragment_title_textView)
+    TextView movieDetailsTitleTextView;
+    @BindView(R.id.movie_details_fragment_rate_textView)
+    TextView movieDetailsRateTextView;
+    @BindView(R.id.movie_details_fragment_release_date_textView)
+    TextView movieDetailsReleaseDateTextView;
+    @BindView(R.id.movie_details_fragment_imageview)
+    ImageView movieDetailsPosterImageView;
+    @BindView(R.id.movie_details_fragment_toolbar)
+    Toolbar movieDetailsToolbar;
 
     MovieDetailsPresenter movieDetailsPresenter;
 
@@ -56,9 +66,13 @@ public class MovieDetailsFragment extends Fragment implements MovieDetailsPresen
     @Override
     public void updateMovieDetailsView(MovieInfo movieInfo) {
         Picasso.get().load(movieInfo.getBackdropPath()).into(collapsingToolbarImageView);
+        Picasso.get().load(movieInfo.getPosterPath()).into(movieDetailsPosterImageView);
         movieDetailsCollapsingToolbar.setTitle(movieInfo.getTitle());
-//        movieDetailsCollapsingToolbar.setExpandedTitleTextAppearance(R.style.Shadow);
         movieDetailsCollapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.transparent));
         movieDetailsFragmentDescriptionTextView.setText(movieInfo.getOverview());
+        movieDetailsTitleTextView.setText(movieInfo.getTitle());
+        movieDetailsRateTextView.setText(movieInfo.getVoteAverage());
+        movieDetailsReleaseDateTextView.setText(movieInfo.getReleaseDate());
+//        movieDetailsToolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_outline_arrow_back_24px_white));
     }
 }
